b60fbe4abb73cec9e3917d9c702450a4c3250805
Jason Shiao <b01901105@ntu.edu.tw>
2022-05-18 16:30:24 +0800

    Test opencv and boost

--- test/main.cpp
+++ test/main.cpp
@@ -1,11 +1,92 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QImage>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <QDebug>
+
+#include <boost/thread.hpp>
+#include <boost/chrono.hpp>
+
+/**
+ * @brief convert cv::Mat to QImage
+ *        WARNING: Use copy() because the data in cv::Mat will be released later. 
+ *                 Without using copy, the data memory is shared between QImage and Mat and 
+ *                 it could cause a crash (invalid memory access)
+ */
+QImage cvMatToQImage (const cv::Mat &inMat) {
+  switch (inMat.type()) {
+    case CV_8UC4:
+      return QImage(inMat.data, inMat.cols, inMat.rows, static_cast<int>(inMat.step), QImage::Format_ARGB32).copy();
+    case CV_8UC3:
+      return QImage(inMat.data, inMat.cols, inMat.rows, static_cast<int>(inMat.step), QImage::Format_RGB888).copy().rgbSwapped();
+    case CV_8UC1:
+      return QImage(inMat.data, inMat.cols, inMat.rows, static_cast<int>(inMat.step), QImage::Format_Grayscale8).copy();
+    default:
+      qWarning() << "cvMatToQImage() failed, the type of mat is: " << inMat.type();
+      break;
+  }
+
+  return QImage();
+}
+
+cv::Mat QImageToCvMat(const QImage &inImage) {
+  switch (inImage.format()) {
+    case QImage::Format_ARGB32: {
+      cv::Mat mat(inImage.height(), inImage.width(),
+                  CV_8UC4,
+                  const_cast<uchar*>(inImage.bits()),
+                  static_cast<size_t>(inImage.bytesPerLine())
+      );
+      return mat;
+    }
+    case QImage::Format_RGB888: {
+      QImage swapped = inImage;
+
+      swapped = swapped.rgbSwapped();
+
+      return (cv::Mat (swapped.height(), swapped.width(),
+                  CV_8UC4,
+                  const_cast<uchar*>(swapped.bits()),
+                  static_cast<size_t>(swapped.bytesPerLine())).clone()
+      );
+    }
+    case QImage::Format_Grayscale8: {
+      cv::Mat mat(inImage.height(), inImage.width(),
+                  CV_8UC1,
+                  const_cast<uchar*>(inImage.bits()),
+                  static_cast<size_t>(inImage.bytesPerLine())
+      );
+      return mat;
+    }
+    default:
+      qWarning() << "QImageToCvMat() failed, the QImage format is: " << inImage.format();
+  }
+
+  return cv::Mat();
+}
+
+void wait(int seconds)
+{
+  boost::this_thread::sleep_for(boost::chrono::seconds{seconds});
+}
+
+void thread()
+{
+  for (int i = 0; i < 5; ++i)
+  {
+    wait(1);
+    //std::cout << i << '\n';
+  }
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    boost::thread t{thread};
+
     return a.exec();
 }
--- test/test.pro
+++ test/test.pro
@@ -4,6 +4,16 @@ greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
 CONFIG += c++17
 
+LIBS += -L"/usr/local/lib/" -lopencv_core
+LIBS += -L"/usr/local/lib/" -lopencv_imgproc
+# OpenCV from Macport
+#LIBS += -L"/opt/local/lib/opencv4/" -lopencv_core
+#LIBS += -L"/opt/local/lib/opencv4/" -lopencv_imgproc
+LIBS += -L"/usr/local/lib/" -lboost_thread-mt
+LIBS += -L"/usr/local/lib/" -lboost_chrono-mt
+INCLUDEPATH += /usr/local/include/
+INCLUDEPATH += /usr/local/include/opencv4/
+
 # You can make your code fail to compile if it uses deprecated APIs.
 # In order to do so, uncomment the following line.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
